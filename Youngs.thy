8623743423c2496555882f8533217b0e291d8d7c
paulson <lp15@cam.ac.uk>
2022-01-07 22:14:00 +0000

    Iff version of the integral lemma

--- Youngs.thy
+++ Youngs.thy
@@ -9,43 +9,50 @@ begin
 
 lemma
   fixes f :: "real \<Rightarrow> real"
-  assumes contf: "continuous_on {0..1} f" and intf: "(f has_integral 0) {0..1}"
+  assumes contf: "continuous_on {0..1} f"
     and f_ge0: "\<And>x. x \<in> {0..1} \<Longrightarrow> f x \<ge> 0"
-  shows "\<forall>x \<in> {0..1}. f x = 0"
-proof (rule ccontr)
-  assume "\<not> (\<forall>x\<in>{0..1}. f x = 0)"
-  then obtain a where a: "a \<in> {0..1}" "f a > 0"
-    using assms by force
-  then obtain \<delta> where "\<delta> > 0" and \<delta>: "\<And>x. x \<in> {0..1} \<Longrightarrow> abs (a - x) < \<delta> \<Longrightarrow> abs (f a - f x) < f a / 2"
-    using contf unfolding continuous_on_real_range
-    by (metis abs_minus_commute half_gt_zero_iff real_norm_def)
-  define l where "l \<equiv> max 0 (a-\<delta>)"
-  define u where "u \<equiv> min 1 (a+\<delta>)"
-  have [simp]: "max 0 l = l" "min 1 u = u" "min l u = l" "max u l = u" 
-               "{0..l} \<union> {u..1} \<union> {l..u} = {0..1}"
-    using a \<open>\<delta> > 0\<close> by (auto simp add: l_def u_def)
-  have "l < u"
-    using \<open>0 < \<delta>\<close> a(1) l_def u_def by auto
-  define I where "I \<equiv> (u-l) * f a / 2"
-  have "I > 0"
-    by (simp add: I_def \<open>l < u\<close> a(2))
-  define g where "g \<equiv> \<lambda>x. if x \<in> {0..l} \<union> {u..1} then 0 else f a / 2"
-  have g_le_f: "g x \<le> f x" if "x \<in> {0..1}" for x
-    using that \<delta>  by (simp add: l_def u_def g_def f_ge0) (smt (verit, best))
-  have "(g has_integral 0) ({0..l} \<union> {u..1})"
-    by (meson g_def has_integral_is_0)
-  moreover  have "((\<lambda>x. f a / 2) has_integral I) {l..u}"
-    unfolding I_def g_def
-    using has_integral_const_real [of "f a / 2" l u] using \<open>l < u\<close> by force
-  then have "(g has_integral I) {l..u}"
-    using has_integral_spike_interior_eq [of l u "\<lambda>x. f a / 2" g I]
-    by (simp add: g_def)
-  ultimately have "(g has_integral I) {0..1}"
-    using has_integral_Un [of g 0 "{0..l} \<union> {u..1}" _ "{l..u}"]
-    by (simp add: Int_Un_distrib2)
-  with g_le_f has_integral_le intf have "I \<le> 0"
-    by blast
-  with \<open>0 < I\<close> show False by fastforce
+  shows "integral {0..1} f = 0 \<longleftrightarrow> (\<forall>x \<in> {0..1}. f x = 0)" (is "?lhs = ?rhs")
+proof
+  assume int0: ?lhs
+  show ?rhs
+  proof (rule ccontr)
+    assume "\<not> (\<forall>x\<in>{0..1}. f x = 0)"
+    then obtain \<xi> where \<xi>: "\<xi> \<in> {0..1}" "f \<xi> > 0"
+      using assms by force
+    then obtain \<delta> where "\<delta> > 0" and \<delta>: "\<And>x. \<lbrakk>x \<in> {0..1}; \<bar>\<xi>-x\<bar> < \<delta>\<rbrakk> \<Longrightarrow> \<bar>f \<xi> - f x\<bar> < f \<xi> / 2"
+      using contf unfolding continuous_on_real_range
+      by (metis abs_minus_commute half_gt_zero_iff real_norm_def)
+    define l where "l \<equiv> max 0 (\<xi>-\<delta>)"
+    define u where "u \<equiv> min 1 (\<xi>+\<delta>)"
+    have [simp]: "max 0 l = l" "min 1 u = u" "min l u = l" "max u l = u" 
+      "{0..l} \<union> {u..1} \<union> {l..u} = {0..1}"
+      using \<xi> \<open>\<delta> > 0\<close> by (auto simp add: l_def u_def)
+    have "l < u"
+      using \<open>0 < \<delta>\<close> \<xi> l_def u_def by auto
+    define I where "I \<equiv> (u-l) * f \<xi> / 2"
+    have "I > 0"
+      by (simp add: I_def \<open>l < u\<close> \<xi>)
+    define g where "g \<equiv> \<lambda>x. if x \<in> {0..l} \<union> {u..1} then 0 else f \<xi> / 2"
+    have g_le_f: "g x \<le> f x" if "x \<in> {0..1}" for x
+      using that \<delta>  by (simp add: l_def u_def g_def f_ge0) (smt (verit, best))
+    have "(g has_integral 0) ({0..l} \<union> {u..1})"
+      by (meson g_def has_integral_is_0)
+    moreover  have "((\<lambda>x. f \<xi> / 2) has_integral I) {l..u}"
+      unfolding I_def g_def
+      using has_integral_const_real [of "f \<xi> / 2" l u] using \<open>l < u\<close> by force
+    then have "(g has_integral I) {l..u}"
+      using has_integral_spike_interior_eq [of l u "\<lambda>x. f \<xi> / 2" g I]
+      by (simp add: g_def)
+    ultimately have "(g has_integral I) {0..1}"
+      using has_integral_Un [of g 0 "{0..l} \<union> {u..1}" _ "{l..u}"]
+      by (simp add: Int_Un_distrib2)
+    with g_le_f has_integral_le  have "I \<le> 0"
+      by (metis contf int0 integrable_continuous_real integrable_integral)
+    with \<open>0 < I\<close> show False by fastforce
+  qed
+next
+  assume ?rhs then show ?lhs
+    by (metis Henstock_Kurzweil_Integration.integral_cong has_integral_const_real has_integral_iff mult_zero_right real_scaleR_def)
 qed
 
 subsection \<open>Possible library additions\<close>
