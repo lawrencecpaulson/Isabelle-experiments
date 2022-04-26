f8ea7f6b1c8d87eba6e3f9e8b98da19aee839cdf
paulson <lp15@cam.ac.uk>
2022-04-26 10:45:02 +0100

    extended version for blog post

--- Misc.thy
+++ Misc.thy
@@ -1,45 +1,17 @@
 section \<open>Misc experiments\<close>
 
 theory Misc imports
-  "HOL-Analysis.Analysis" 
+  "HOL-Decision_Procs.Approximation"
   "HOL-ex.Sketch_and_Explore"
    
 begin
 
-lemma "\<not> (\<exists>q::rat. q^2 = 2)"
-proof
-  assume "\<exists>q::rat. q^2 = 2"
-  then obtain q::rat where "q^2 = 2"
-    by blast 
-  obtain m n where "coprime m n" "q = of_int m / of_int n"
-    by (meson quotient_of_coprime quotient_of_div surj_pair)
-  then have "of_int m ^ 2 / of_int n ^ 2 = (2::rat)"
-    by (metis \<open>q\<^sup>2 = 2\<close> power_divide)
-  then have 2: "of_int m ^ 2 = (2::rat) * of_int n ^ 2"
-    by (metis division_ring_divide_zero double_eq_0_iff mult_2_right mult_zero_right nonzero_divide_eq_eq)
-  then have "2 dvd m"
-    by (metis (mono_tags, lifting) even_mult_iff even_numeral of_int_eq_iff of_int_mult of_int_numeral power2_eq_square)
-  then obtain r where "m = 2*r"
-    by blast
-  then have "2 dvd n"
-    by (smt (verit, ccfv_threshold) "2" Groups.mult_ac(3) dvd_def even_mult_iff mult_cancel_left of_int_1 of_int_add of_int_eq_of_int_power_cancel_iff of_int_mult one_add_one power2_eq_square)
-  then show False
-    using \<open>coprime m n\<close> \<open>m = 2 * r\<close> by auto
-qed
+lemma "\<lbrakk>0 < N; 0 \<le> x; x < 1\<rbrakk> \<Longrightarrow>  \<lfloor>x * real_of_int N\<rfloor> < N"
+  by (simp add: floor_less_iff)
 
+lemma "\<lbrakk>0 < N; 0 \<le> x; x < 1\<rbrakk> \<Longrightarrow> nat \<lfloor>x * real N\<rfloor> < N"
+  by (smt (verit) mult_less_cancel_right2 mult_nonneg_nonneg of_nat_0_less_iff of_nat_floor of_nat_less_imp_less)
 
-lemma "Nats \<noteq> (Ints :: complex set)"
-  by (smt (z3) Ints_minus Nats_altdef1 mem_Collect_eq of_int_eq_iff of_int_minus)
-
-lemma "Nats \<noteq> (Ints :: real set)"
-  by (smt (z3) Ints_minus Nats_altdef1 mem_Collect_eq of_int_eq_iff of_int_minus)
-
-thm of_nat_def Int.of_int_def
-
-lemma "Nats \<noteq> (Ints :: 'a::linordered_idom set)"
-  by (metis Ints_1 Ints_minus Nats_induct neg_0_le_iff_le not_one_le_zero of_nat_0_le_iff)
-
-lemma "m * 2^(2*m) \<le> 2^(2^m)"
 
 text \<open>Kevin Buzzard's examples\<close>
 lemma
@@ -59,6 +31,73 @@ proof -
   then show ?thesis
     by (simp add: real_less_rsqrt)
 qed
+
+lemma "sqrt 2 + sqrt 3 < sqrt 10"
+  by (approximation 10)
+
+lemma "3.141592635389 < pi"
+  by (approximation 30)
+
+lemma "sqrt 2 \<notin> \<rat>"
+proof
+  assume "sqrt 2 \<in> \<rat>"
+  then obtain q::rat where "sqrt 2 = of_rat q"
+    using Rats_cases by blast
+  then have "q^2 = 2"
+    by (metis abs_numeral of_rat_eq_iff of_rat_numeral_eq of_rat_power power2_eq_square real_sqrt_mult_self)
+  then obtain m n
+    where "coprime m n" "q = of_int m / of_int n"
+    by (metis Fract_of_int_quotient Rat_cases)
+  then have "of_int m ^ 2 / of_int n ^ 2 = (2::rat)"
+    by (metis \<open>q\<^sup>2 = 2\<close> power_divide)
+  then have 2: "of_int m ^ 2 = (2::rat) * of_int n ^ 2"
+    by (metis division_ring_divide_zero double_eq_0_iff mult_2_right mult_zero_right nonzero_divide_eq_eq)
+  then have "2 dvd m"
+    by (metis (mono_tags, lifting) even_mult_iff even_numeral of_int_eq_iff of_int_mult of_int_numeral power2_eq_square)
+  then obtain r where "m = 2*r"
+    by blast
+  then have "2 dvd n"
+    by (smt (verit) "2" \<open>even m\<close> dvdE even_mult_iff mult.left_commute mult_cancel_left of_int_1 of_int_add of_int_eq_iff of_int_mult one_add_one power2_eq_square)
+  then show False
+    using \<open>coprime m n\<close> \<open>m = 2 * r\<close> by simp
+qed
+
+
+
+
+lemma "\<not> (\<exists>q::rat. q^2 = 2)"
+proof
+  assume "\<exists>q::rat. q^2 = 2"
+  then obtain q::rat and m n
+    where "q^2 = 2" "coprime m n" "q = of_int m / of_int n"
+    by (meson quotient_of_coprime quotient_of_div surj_pair)
+  then have "of_int m ^ 2 / of_int n ^ 2 = (2::rat)"
+    by (metis \<open>q\<^sup>2 = 2\<close> power_divide)
+  then have 2: "of_int m ^ 2 = (2::rat) * of_int n ^ 2"
+    by (metis division_ring_divide_zero double_eq_0_iff mult_2_right mult_zero_right nonzero_divide_eq_eq)
+  then have "2 dvd m"
+    by (metis (mono_tags, lifting) even_mult_iff even_numeral of_int_eq_iff of_int_mult of_int_numeral power2_eq_square)
+  then obtain r where "m = 2*r"
+    by blast
+  then have "2 dvd n"
+    by (smt (verit, ccfv_threshold) "2" Groups.mult_ac(3) dvd_def even_mult_iff mult_cancel_left of_int_1 of_int_add of_int_eq_of_int_power_cancel_iff of_int_mult one_add_one power2_eq_square)
+  then show False
+    using \<open>coprime m n\<close> \<open>m = 2 * r\<close> by simp
+qed
+
+lemma "Nats \<noteq> (Ints :: complex set)"
+  by (smt (z3) Ints_minus Nats_altdef1 mem_Collect_eq of_int_eq_iff of_int_minus)
+
+lemma "Nats \<noteq> (Ints :: real set)"
+  by (smt (z3) Ints_minus Nats_altdef1 mem_Collect_eq of_int_eq_iff of_int_minus)
+
+thm of_nat_def Int.of_int_def
+
+lemma "Nats \<noteq> (Ints :: 'a::linordered_idom set)"
+  by (metis Ints_1 Ints_minus Nats_induct neg_0_le_iff_le not_one_le_zero of_nat_0_le_iff)
+
+lemma "m * 2^(2*m) \<le> 2^(2^m)"
+  oops
 
 
 lemma
