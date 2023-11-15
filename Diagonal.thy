8995d82714994460323f701410b74a46ab6873f5
paulson <lp15@cam.ac.uk>
2023-11-15 12:39:00 +0000

    Definitions to the end of page 9

--- Diagonal.thy
+++ Diagonal.thy
@@ -17,13 +17,14 @@ definition Neighbours :: "'a set set \<Rightarrow> 'a \<Rightarrow> 'a set" wher
 locale Diagonal = fin_sgraph +   \<comment> \<open>finite simple graphs (no loops)\<close>
   fixes k::nat       \<comment> \<open>red limit\<close>
   fixes l::nat       \<comment> \<open>blue limit\<close>
-  assumes lk: "l \<le> k"
+  assumes ln0: "0 < l" and lk: "l \<le> k" 
   assumes complete: "E \<equiv> all_edges V"
   fixes Red Blue
   assumes Red_not_Blue: "Red \<noteq> Blue"
   assumes part_RB: "partition_on E {Red,Blue}"
   assumes no_Red_clique: "\<not> (\<exists>K. size_clique k K Red)"
   assumes no_Blue_clique: "\<not> (\<exists>K. size_clique l K Blue)"
+  fixes X0 :: "'a set" and Y0 :: "'a set"    \<comment> \<open>initial values\<close>
 begin
 
 abbreviation "nV \<equiv> card V"
@@ -40,11 +41,28 @@ lemma disjnt_Red_Blue: "disjnt Red Blue"
 lemma nontriv: "E \<noteq> {}"
   using Red_E bot.extremum_strict by blast
 
+lemma kn0: "k > 0"
+  using lk ln0 by auto
+
 text \<open>for calculating the perimeter p\<close>
 definition "red_density X Y \<equiv> card (Red \<inter> mk_edge ` all_edges_between X Y) / (card X * card Y)"
 
 lemma red_density_ge0: "red_density X Y \<ge> 0"
   by (auto simp: red_density_def)
+
+lemma red_le_edge_density: "red_density X Y \<le> edge_density X Y"
+proof (cases "finite X \<and> finite Y")
+  case True
+  then have "card (Red \<inter> mk_edge ` all_edges_between X Y) \<le> card (mk_edge ` all_edges_between X Y)"
+    by (simp add: card_mono finite_all_edges_between)
+  also have "... \<le> card (all_edges_between X Y)"
+    by (meson card_image_le finite_all_edges_between')
+  finally show ?thesis
+    by (simp add: red_density_def edge_density_def divide_right_mono)
+qed (auto simp: red_density_def edge_density_def)
+
+lemma red_density_le1: "red_density X Y \<le> 1"
+  by (meson edge_density_le1 order_trans red_le_edge_density)
 
 definition Weight :: "'a set \<Rightarrow> 'a set \<Rightarrow> 'a \<Rightarrow> 'a \<Rightarrow> real" where
   "Weight \<equiv> \<lambda>X Y x y. inverse (card Y) * (card (Neighbours Red x \<inter> Neighbours Red y \<inter> Y)
@@ -52,3 +70,41 @@ definition Weight :: "'a set \<Rightarrow> 'a set \<Rightarrow> 'a \<Rightarrow>
 
 definition weight :: "'a set \<Rightarrow> 'a set \<Rightarrow> 'a \<Rightarrow> real" where
   "weight \<equiv> \<lambda>X Y x. \<Sum>y \<in> X-{x}. Weight X Y x y"
+
+definition "p0 \<equiv> red_density X0 Y0"
+
+definition "epsk \<equiv> k powr (-1/4)"
+
+definition "q \<equiv> \<lambda>h. p0 + ((1 + epsk)^h - 1) / k"
+
+definition "height \<equiv> \<lambda>p. LEAST h. p \<le> q h \<and> h>0"
+
+lemma q0 [simp]: "q 0 = p0"
+  by (simp add: q_def)
+
+lemma p0_01: "0 \<le> p0" "p0 \<le> 1"
+  by (simp_all add: p0_def red_density_ge0 red_density_le1)
+
+lemma epsk_n0: "epsk > 0"
+  by (auto simp: epsk_def kn0)
+
+lemma height_exists:
+  assumes "0 < p" and "p < 1"
+  obtains h where "p \<le> q h"
+proof -
+  let ?h = "nat \<lceil>k / epsk\<rceil>"  \<comment>\<open>larger than the bound suggested in the paper\<close>
+  have "k+1 \<le> (1 + epsk) ^ ?h"
+    using linear_plus_1_le_power [of epsk ?h] epsk_n0
+    by (smt (verit, best) mult_imp_less_div_pos of_nat_1 of_nat_add of_nat_ceiling)
+  then have "p \<le> q ?h"
+    unfolding q_def
+    using kn0 assms p0_01
+    by (smt (verit, best) le_divide_eq_1_pos of_nat_0_less_iff of_nat_1 of_nat_add)
+  then show thesis ..
+qed
+
+lemma q_Suc_diff: "q(Suc h) - q h = epsk * (1 + epsk)^h / k"
+  by (simp add: q_def field_split_simps)
+
+definition "alpha \<equiv> \<lambda>h. q h - q (h-1)"
+
