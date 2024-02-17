405b45bfdf2498bdb9363cdd2a9950616b129da3
paulson <lp15@cam.ac.uk>
2024-02-17 14:54:07 +0000

    Removal of small examples by Fields medallists to use in a blogpost

--- Misc.thy
+++ Misc.thy
@@ -6,44 +6,6 @@ theory Misc imports
    
 begin
 
-abbreviation set_difference :: "['a set,'a set] \<Rightarrow> 'a set" (infixl "\<setminus>" 65)
-  where "A \<setminus> B \<equiv> A-B"
-
-text \<open>Tim Gowers' example\<close>
-lemma "bij_betw f X Y \<longleftrightarrow> (\<forall>A. A\<subseteq>X \<longrightarrow> f ` (X\<setminus>A) = Y \<setminus> f`A)"
-  by (metis Diff_empty Diff_eq_empty_iff Diff_subset bij_betw_def image_is_empty 
-            inj_on_image_set_diff subset_antisym subset_image_inj)
-
-lemma "bij_betw f X Y \<longleftrightarrow> (\<forall>A. A\<subseteq>X \<longrightarrow> f ` (X\<setminus>A) = Y \<setminus> f`A)"  (is "?L=?R")
-proof
-  show "?L \<Longrightarrow> ?R"
-    by (metis Diff_subset bij_betw_def inj_on_image_set_diff)
-  assume ?R
-  then have "inj_on f X" "f ` X = Y"
-    by (auto simp: inj_on_def)
-  then show ?L
-    by (simp add: bij_betw_def)
-qed
-
-
-(*Terrence Tao's example*)
-lemma
-  fixes a :: "nat \<Rightarrow> real"
-  assumes a: "decseq a" and D: "\<And>k. D k \<ge> 0" and aD: "\<And>k. a k \<le> D k - D(Suc k)"
-  shows "a k \<le> D 0 / (Suc k)"
-proof -
-  have "a k = (\<Sum>i\<le>k. a k) / (Suc k)"
-    by simp
-  also have "\<dots> \<le> (\<Sum>i\<le>k. a i) / (Suc k)"
-    using a sum_mono[of "{..k}" "\<lambda>i. a k" a] 
-    by (simp add: monotone_def divide_simps mult.commute)
-  also have "\<dots> \<le> (\<Sum>i\<le>k. D i - D(Suc i)) / (Suc k)"
-    by (simp add: aD divide_right_mono sum_mono)
-  also have "\<dots> \<le> D 0 / (Suc k)"
-    by (simp add: sum_telescope D divide_right_mono)
-  finally show ?thesis .
-qed
-
 
 lemma DD: "x>(0::real) \<Longrightarrow> y>0 \<Longrightarrow> x powr (ln y) = y powr (ln x)"
   by (simp add: powr_def)
