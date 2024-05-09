fdd94bf2278f407fadacd6c2e307e86064d5c4fd
paulson <lp15@cam.ac.uk>
2024-05-09 16:14:40 +0100

    Progress on Lemma 10.2, but a new sorry

--- Closer_To_Diagonal.thy
+++ Closer_To_Diagonal.thy
@@ -31,6 +31,11 @@ lemma (in Book) Far_10_2_aux:
   assumes big: "Big_Far_10_2 \<gamma> l"
   shows False
 proof -
+
+  have bull: "ok_fun_95b k \<ge> 0"
+  (* this is simply false so we need to figure out why it seems to be necessary*)
+    sorry
+
   define \<R> where "\<R> \<equiv> Step_class \<gamma> l k {red_step}"
   define t where "t \<equiv> card \<R>"
   define m where "m \<equiv> halted_point \<gamma> l k"
@@ -58,7 +63,7 @@ proof -
       using \<gamma>_def big93 by blast
   qed (use assms in auto)
 
-  have "exp (-(\<gamma>/20) * k + ok_fun_95b k) \<le> exp (-\<delta> * k + ok_fun_95b k)"
+  have "exp (-(\<gamma>/20) * k + ok_fun_95b k) \<le> exp (-\<delta> * k + ok_fun_95b k)"  (*WHY IS THIS HERE*)
     using \<gamma>01 apply (simp add: \<delta>_def)
     by (metis Groups.mult_ac(3) \<gamma>(1) divide_le_eq_numeral1(1) mult_less_cancel_left2 of_nat_0_le_iff vector_space_over_itself.scale_scale verit_comp_simplify(3))
 
@@ -78,9 +83,69 @@ proof -
   then have 52: "card (Yseq \<gamma> l k m) \<ge> 
                exp (-\<delta> * k + ok_fun_95b k) * (1-\<gamma>) powr (\<gamma>*t / (1-\<gamma>)) * exp (\<gamma> * (real t)\<^sup>2 / (2*k)) * (k-t+l choose l)"
     using \<gamma> by simp
+
+  define gamf where "gamf \<equiv> \<lambda>x::real. (1-x) powr (1/(1-x))"
+
+  have deriv_gamf: "\<exists>y. DERIV gamf x :> y \<and> y \<le> 0" if "0<a" "a\<le>x" "x\<le>b" "b<1" for a b x
+    unfolding gamf_def
+    apply (rule exI conjI DERIV_powr derivative_eq_intros | use that in force)+
+    using that
+    apply (simp add: mult_le_0_iff)
+    by (smt (verit, ccfv_SIG) frac_le ln_less_self zero_less_mult_iff)
+
   have "(k-t+l choose l) \<le> 
         exp (-\<delta> * k + ok_fun_95b k) * (1-\<gamma>) powr (\<gamma>*t / (1-\<gamma>)) * exp (\<gamma> * (real t)\<^sup>2 / (2*k)) * (k-t+l choose l)"
-    sorry
+  proof (cases "\<gamma> > 3/20")
+    case True
+    then have "\<delta> * k < (\<gamma>/30) * k"
+      using \<open>k>0\<close> by (simp add: \<delta>_def)
+    also have "\<dots> \<le> 3 * \<gamma> * (real t)\<^sup>2 / (40*k)"
+      using True mult_right_mono [OF mult_mono [OF t23 t23], of "3*\<gamma> / (40*k)"] \<open>k>0\<close> by (simp add: power2_eq_square)
+    finally have \<dagger>: "\<delta>*k < 3 * \<gamma> * (real t)\<^sup>2 / (40*k)" .
+    have "gamf \<gamma> \<ge> gamf (1/5)"
+      by (smt (verit, best) DERIV_nonpos_imp_nonincreasing[of \<gamma> "1/5" gamf] \<gamma> \<gamma>01 deriv_gamf divide_less_eq_1)
+    moreover have "ln (gamf (1/5)) \<ge> -1/3 + 1/20"
+      unfolding gamf_def by (approximation 10)
+    moreover have "gamf (1/5) > 0"
+      by (simp add: gamf_def)
+    ultimately have "gamf \<gamma> \<ge> exp (-1/3 + 1/20)"
+      using ln_ge_iff by auto
+    from powr_mono2 [OF _ _ this]
+    have "(1-\<gamma>) powr (\<gamma>*t / (1-\<gamma>)) \<ge> exp (-17/60) powr (\<gamma>*t)"
+      unfolding gamf_def using \<gamma>01 powr_powr by fastforce
+    from mult_left_mono [OF this, of "exp (\<gamma> * (real t)\<^sup>2 / (2*k))"]
+    have "(1-\<gamma>) powr (\<gamma>*t / (1-\<gamma>)) * exp (\<gamma> * (real t)\<^sup>2 / (2*k)) \<ge> exp (-17/60 * (\<gamma>*t) + (\<gamma> * (real t)\<^sup>2 / (2*k)))"
+      by (smt (verit) Groups.mult_ac(2) exp_add exp_ge_zero exp_powr_real)
+    moreover have "(-17/60 * (\<gamma>*t) + (\<gamma> * (real t)\<^sup>2 / (2*k))) \<ge> (3*\<gamma> * (real t)\<^sup>2 / (40*k))"
+      using t23 \<open>k>0\<close> \<open>\<gamma>>0\<close> by (simp add: divide_simps eval_nat_numeral)
+    ultimately have "(1-\<gamma>) powr (\<gamma>*t / (1-\<gamma>)) * exp (\<gamma> * (real t)\<^sup>2 / (2*k)) \<ge> exp (3*\<gamma> * (real t)\<^sup>2 / (40*k))"
+      by (smt (verit) exp_mono)
+    with \<dagger> have "(1-\<gamma>) powr (\<gamma>*t / (1-\<gamma>)) * exp (\<gamma> * (real t)\<^sup>2 / (2*k)) \<ge> exp (\<delta>*k)"
+      by (smt (verit, best) exp_le_cancel_iff)
+    then have "1 \<le> exp (-\<delta>*k + ok_fun_95b k) * (1 - \<gamma>) powr (\<gamma> * real t / (1 - \<gamma>)) * exp (\<gamma> * (real t)\<^sup>2 / real (2 * k))"
+      using bull
+      apply (simp add: exp_add exp_diff)
+      apply (erule order.trans)
+      by (simp add: mult_le_cancel_right1 mult_right_mono)
+    then show ?thesis by auto
+  next
+    case False
+    have "\<delta> * k < (\<gamma> / 15) * k"
+      using \<gamma> \<open>k>0\<close> by (simp add: \<delta>_def)
+    also have "\<dots> \<le> 3 * \<gamma> * (real t)\<^sup>2 / (20*k)"
+      using \<gamma> mult_right_mono [OF mult_mono [OF t23 t23], of "3*\<gamma> / (20*k)"] \<open>k>0\<close> by (simp add: power2_eq_square)
+    finally have \<dagger>: "\<delta>*k < 3 * \<gamma> * (real t)\<^sup>2 / (20*k)" .
+    have "gamf \<gamma> \<ge> gamf (3/20)"
+      by (smt (verit, best) DERIV_nonpos_imp_nonincreasing[of \<gamma> "3/20" gamf] \<gamma>01 deriv_gamf False divide_less_eq_1)
+    moreover have "ln (gamf (3/20)) \<ge> -1/3 + 1/10"
+      unfolding gamf_def by (approximation 10)
+    moreover have "gamf (3/20) > 0"
+      by (simp add: gamf_def)
+    ultimately have "gamf \<gamma> \<ge> exp (-1/3 + 1/10)"
+      using ln_ge_iff by auto
+
+    then show ?thesis sorry
+  qed
   with 52 have "(k-t+l choose l) \<le> card (Yseq \<gamma> l k m)" by linarith
   then show False
     using Far_9_2_conclusion [OF \<open>Colours l k\<close> \<gamma>01] by (simp flip: \<R>_def m_def t_def)
