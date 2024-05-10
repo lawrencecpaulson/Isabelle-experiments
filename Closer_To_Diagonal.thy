17d473ea53cc643f71dabac7d434ad8fe445ec5e
paulson <lp15@cam.ac.uk>
2024-05-10 19:02:14 +0100

    Finally a full proof of 10.2 including bigness, but UGLY

--- Closer_To_Diagonal.thy
+++ Closer_To_Diagonal.thy
@@ -7,8 +7,9 @@ begin
 
 subsection \<open>Lemma 10.2\<close>
 
-(*The body of the proof is there in full, but the "sufficiently large" assumptions are problematical.
-The calculation for \<gamma> > 3/20 is sharp. We seem to need a finite gap for the limit to exist. *)
+(*The "sufficiently large" assumptions are problematical.
+  The calculation for \<gamma> > 3/20 is sharp. We need a finite gap for the limit to exist. 
+  We can get away with 1/300. *)
 
 context P0_min
 begin
@@ -30,31 +31,51 @@ proof -
     by (simp add: algebra_simps not_less abs_if add_increasing split: if_split_asm)
 qed
 
+lemma error_10_2_True: "\<forall>\<^sup>\<infinity>k. ok_fun_95b k + (3/20 + 1/300) * real k / real 30 \<ge> k/200"
+  by (intro error_10_2) auto
+
+lemma error_10_2_False: "\<forall>\<^sup>\<infinity>k. ok_fun_95b k + (1/10) * real k / real 15 \<ge> k/200"
+  by (intro error_10_2) auto
+
 definition "Big_Far_10_2 \<equiv> \<lambda>\<mu> l. Big_Far_9_3 \<mu> l \<and> Big_Far_9_5 \<mu> l
-                \<and> (\<forall>k\<ge>l. ok_fun_95b k + (if \<mu> > 3/20 then \<mu>*k/30 else \<mu>*k/15) \<ge> k/200)"
+                \<and> (\<forall>k\<ge>l. ok_fun_95b k + (if \<mu> > (3/20 + 1/300) then \<mu>*k/30 else \<mu>*k/15) \<ge> k/200)"
 
 lemma Big_Far_10_2:
   assumes "1/10\<le>\<mu>1" "\<mu>1<1" 
   shows "\<forall>\<^sup>\<infinity>l. \<forall>\<mu>. 1/10 \<le> \<mu> \<and> \<mu> \<le> \<mu>1 \<longrightarrow> Big_Far_10_2 \<mu> l"
 proof -
-  have "\<forall>\<^sup>\<infinity>l. \<forall>k\<ge>l. (\<forall>\<mu>. 1/10 \<le> \<mu> \<and> \<mu> \<le> \<mu>1 \<longrightarrow> k/200 \<le> ok_fun_95b k + (if \<mu> > 3/20 then \<mu>*k/30 else \<mu>*k/15))"
+  have T: "\<forall>\<^sup>\<infinity>l. \<forall>k\<ge>l. (\<forall>\<mu>. (3/20 + 1/300) \<le> \<mu> \<and> \<mu> \<le> \<mu>1 \<longrightarrow> k/200 \<le> ok_fun_95b k + \<mu>*k / real 30)"
     using assms 
+    apply (intro eventually_all_ge_at_top eventually_all_geI0 error_10_2_True)
+    apply (auto simp: divide_right_mono mult_right_mono elim!: order_trans)
+    by (metis Groups.mult_ac(2) more_arith_simps(11) mult_right_mono of_nat_0_le_iff)
+  have F: "\<forall>\<^sup>\<infinity>l. \<forall>k\<ge>l. (\<forall>\<mu>. 1/10 \<le> \<mu> \<and> \<mu> \<le> \<mu>1 \<longrightarrow> k/200 \<le> ok_fun_95b k + \<mu>*k / real 15)"
+    using assms 
+    apply (intro eventually_all_ge_at_top eventually_all_geI0 error_10_2_False)
+    apply (auto simp: divide_right_mono mult_right_mono elim!: order_trans)
+    using Suc_n_not_le_n
+    by (metis Groups.mult_ac(3) mult_le_cancel_left1 mult_of_nat_commute of_nat_less_0_iff)
+  have "\<forall>\<^sup>\<infinity>l. \<forall>k\<ge>l. (\<forall>\<mu>. 1/10 \<le> \<mu> \<and> \<mu> \<le> \<mu>1 \<longrightarrow> k/200 \<le> ok_fun_95b k + (if \<mu> > (3/20 + 1/300) then \<mu>*k/30 else \<mu>*k/15))"
+    using assms
     apply (split if_split)
     unfolding eventually_conj_iff all_imp_conj_distrib all_conj_distrib imp_conjL [symmetric]
-    apply (intro conjI eventually_all_ge_at_top eventually_all_geI0 )
-     apply (rule eventually_mono [OF error_10_2])
-
-     apply (auto simp: divide_right_mono mult_right_mono elim!: order_trans)
-    sorry
+    apply (rule )
+     apply (rule eventually_mono [OF T])
+     apply (force simp add: )
+    apply (rule eventually_mono [OF F])
+    apply (force simp add: )
     done
   then show ?thesis
-    using assms Big_Far_9_3 Big_Far_9_5
+    using assms Big_Far_9_3[of "1/10"] Big_Far_9_5[of "1/10"]
     unfolding Big_Far_10_2_def 
-    apply (simp add: eventually_conj_iff all_imp_conj_distrib)  
-    oops
-    by (smt (verit, ccfv_threshold) eventually_sequentially)
+    unfolding eventually_conj_iff all_imp_conj_distrib all_conj_distrib imp_conjL [symmetric]
+    apply (intro conjI)
+      apply (simp add: )
+     apply (simp add: )
+    apply (erule eventually_mono)
+    apply (force simp add: )
+    done
 qed
-
 
 
 text \<open>A little tricky to express since my "Colours" assumption includes the allowed 
@@ -126,7 +147,7 @@ proof -
     by (smt (verit, del_insts) divide_le_0_iff ln_one_minus_pos_upper_bound mult_le_0_iff powr_ge_pzero that)
 
   have "(1-\<gamma>) powr (\<gamma>*t / (1-\<gamma>)) * exp (\<gamma> * (real t)\<^sup>2 / (2*k)) \<ge> exp (\<delta>*k - ok_fun_95b k)"
-  proof (cases "\<gamma> > 3/20")
+  proof (cases "\<gamma> > 3/20 + 1/300")
     case True
     then have "\<delta> * k - ok_fun_95b k \<le> (\<gamma>/30) * k"
       using \<open>k>0\<close> ok95 by (simp add: \<delta>_def)
@@ -156,7 +177,7 @@ proof -
       by (smt (verit, best) exp_le_cancel_iff)
   next
     case False
-    then have "\<delta> * k - ok_fun_95b k \<le> (\<gamma>/30) * k"
+    have "\<delta> * k - ok_fun_95b k \<le> (\<gamma>/30) * k"
       using \<open>k>0\<close> ok95 by (simp add: \<delta>_def)
     also have "\<dots> \<le> 3 * \<gamma> * (real t)\<^sup>2 / (40*k)"
       using \<gamma> mult_right_mono [OF mult_mono [OF t23 t23], of "3*\<gamma> / (40*k)"] \<open>k>0\<close>
@@ -165,11 +186,16 @@ proof -
       using \<gamma> by (simp add: divide_simps)
     finally have \<dagger>: "\<delta>*k - ok_fun_95b k \<le> 3 * \<gamma> * (real t)\<^sup>2 / (20*k)" .
 
-    have "gamf \<gamma> \<ge> gamf (3/20)"
-      by (smt (verit, best) DERIV_nonpos_imp_nonincreasing[of \<gamma> "3/20" gamf] \<gamma>01 deriv_gamf False divide_less_eq_1)
-    moreover have "ln (gamf (3/20)) \<ge> -1/3 + 1/10"
+    have "gamf \<gamma> \<ge> gamf (3/20 + 1/300)"
+      apply (intro DERIV_nonpos_imp_nonincreasing[of \<gamma> "3/20 + 1/300" gamf] )
+      using False apply (force simp add: )
+      apply (rule deriv_gamf [of \<gamma>])
+      using False \<gamma>
+         apply (auto simp: )
+      done
+    moreover have "ln (gamf (3/20 + 1/300)) \<ge> -1/3 + 1/10"
       unfolding gamf_def by (approximation 6)
-    moreover have "gamf (3/20) > 0"
+    moreover have "gamf (3/20 + 1/300) > 0"
       by (simp add: gamf_def)
     ultimately have "gamf \<gamma> \<ge> exp (-1/3 + 1/10)"
       using ln_ge_iff by auto
