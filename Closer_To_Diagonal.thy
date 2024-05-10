87f07125ae842786f1612a2c533011e7e269980d
paulson <lp15@cam.ac.uk>
2024-05-10 19:21:15 +0100

    Tidied up Lemma 10.2 and corrected the proofs

--- Closer_To_Diagonal.thy
+++ Closer_To_Diagonal.thy
@@ -7,9 +7,6 @@ begin
 
 subsection \<open>Lemma 10.2\<close>
 
-(*The "sufficiently large" assumptions are problematical.
-  The calculation for \<gamma> > 3/20 is sharp. We need a finite gap for the limit to exist. 
-  We can get away with 1/300. *)
 
 context P0_min
 begin
@@ -31,23 +28,29 @@ proof -
     by (simp add: algebra_simps not_less abs_if add_increasing split: if_split_asm)
 qed
 
-lemma error_10_2_True: "\<forall>\<^sup>\<infinity>k. ok_fun_95b k + (3/20 + 1/300) * real k / real 30 \<ge> k/200"
+text \<open>The "sufficiently large" assumptions are problematical.
+  The proof's calculation for @{term "\<gamma> > 3/20"} is sharp. 
+  We need a finite gap for the limit to exist.  We can get away with 1/300.\<close>
+definition x320::real where "x320 \<equiv> 3/20 + 1/300"
+
+lemma error_10_2_True: "\<forall>\<^sup>\<infinity>k. ok_fun_95b k + x320 * real k / real 30 \<ge> k/200"
+  unfolding x320_def
   by (intro error_10_2) auto
 
 lemma error_10_2_False: "\<forall>\<^sup>\<infinity>k. ok_fun_95b k + (1/10) * real k / real 15 \<ge> k/200"
   by (intro error_10_2) auto
 
 definition "Big_Far_10_2 \<equiv> \<lambda>\<mu> l. Big_Far_9_3 \<mu> l \<and> Big_Far_9_5 \<mu> l
-                \<and> (\<forall>k\<ge>l. ok_fun_95b k + (if \<mu> > (3/20 + 1/300) then \<mu>*k/30 else \<mu>*k/15) \<ge> k/200)"
+                \<and> (\<forall>k\<ge>l. ok_fun_95b k + (if \<mu> > x320 then \<mu>*k/30 else \<mu>*k/15) \<ge> k/200)"
 
 lemma Big_Far_10_2:
   assumes "1/10\<le>\<mu>1" "\<mu>1<1" 
   shows "\<forall>\<^sup>\<infinity>l. \<forall>\<mu>. 1/10 \<le> \<mu> \<and> \<mu> \<le> \<mu>1 \<longrightarrow> Big_Far_10_2 \<mu> l"
 proof -
-  have T: "\<forall>\<^sup>\<infinity>l. \<forall>k\<ge>l. (\<forall>\<mu>. (3/20 + 1/300) \<le> \<mu> \<and> \<mu> \<le> \<mu>1 \<longrightarrow> k/200 \<le> ok_fun_95b k + \<mu>*k / real 30)"
+  have T: "\<forall>\<^sup>\<infinity>l. \<forall>k\<ge>l. (\<forall>\<mu>. x320 \<le> \<mu> \<and> \<mu> \<le> \<mu>1 \<longrightarrow> k/200 \<le> ok_fun_95b k + \<mu>*k / real 30)"
     using assms 
     apply (intro eventually_all_ge_at_top eventually_all_geI0 error_10_2_True)
-    apply (auto simp: divide_right_mono mult_right_mono elim!: order_trans)
+    apply (auto simp: x320_def divide_right_mono mult_right_mono elim!: order_trans)
     by (metis Groups.mult_ac(2) more_arith_simps(11) mult_right_mono of_nat_0_le_iff)
   have F: "\<forall>\<^sup>\<infinity>l. \<forall>k\<ge>l. (\<forall>\<mu>. 1/10 \<le> \<mu> \<and> \<mu> \<le> \<mu>1 \<longrightarrow> k/200 \<le> ok_fun_95b k + \<mu>*k / real 15)"
     using assms 
@@ -55,7 +58,7 @@ proof -
     apply (auto simp: divide_right_mono mult_right_mono elim!: order_trans)
     using Suc_n_not_le_n
     by (metis Groups.mult_ac(3) mult_le_cancel_left1 mult_of_nat_commute of_nat_less_0_iff)
-  have "\<forall>\<^sup>\<infinity>l. \<forall>k\<ge>l. (\<forall>\<mu>. 1/10 \<le> \<mu> \<and> \<mu> \<le> \<mu>1 \<longrightarrow> k/200 \<le> ok_fun_95b k + (if \<mu> > (3/20 + 1/300) then \<mu>*k/30 else \<mu>*k/15))"
+  have "\<forall>\<^sup>\<infinity>l. \<forall>k\<ge>l. (\<forall>\<mu>. 1/10 \<le> \<mu> \<and> \<mu> \<le> \<mu>1 \<longrightarrow> k/200 \<le> ok_fun_95b k + (if \<mu> > x320 then \<mu>*k/30 else \<mu>*k/15))"
     using assms
     apply (split if_split)
     unfolding eventually_conj_iff all_imp_conj_distrib all_conj_distrib imp_conjL [symmetric]
@@ -119,9 +122,6 @@ proof -
       using \<gamma>_def big93 by blast
   qed (use assms in auto)
 
-  have ok95: "ok_fun_95b k + \<gamma> * real k / 30 \<ge> k/200"
-    using big \<open>k\<ge>l\<close> by (auto simp: Big_Far_10_2_def Big_Far_9_2_def)
-
   have "card (Yseq \<gamma> l k m) \<ge> 
                exp (-\<delta> * k + ok_fun_95b k) * (1-\<gamma>) powr (\<gamma>*t / (1-\<gamma>)) * ((1-\<gamma>)/(1-\<gamma>))^t 
              * exp (\<gamma> * (real t)\<^sup>2 / (2*k)) * (k-t+l choose l)"
@@ -147,12 +147,15 @@ proof -
     by (smt (verit, del_insts) divide_le_0_iff ln_one_minus_pos_upper_bound mult_le_0_iff powr_ge_pzero that)
 
   have "(1-\<gamma>) powr (\<gamma>*t / (1-\<gamma>)) * exp (\<gamma> * (real t)\<^sup>2 / (2*k)) \<ge> exp (\<delta>*k - ok_fun_95b k)"
-  proof (cases "\<gamma> > 3/20 + 1/300")
+  proof (cases "\<gamma> > x320")
     case True
-    then have "\<delta> * k - ok_fun_95b k \<le> (\<gamma>/30) * k"
-      using \<open>k>0\<close> ok95 by (simp add: \<delta>_def)
+    then have "ok_fun_95b k + \<gamma>*k / 30 \<ge> k/200"
+      using big \<open>k\<ge>l\<close> by (auto simp: Big_Far_10_2_def Big_Far_9_2_def)
+    with True \<open>k>0\<close> have "\<delta> * k - ok_fun_95b k \<le> (\<gamma>/30) * k"
+      by (simp add: \<delta>_def)
     also have "\<dots> \<le> 3 * \<gamma> * (real t)\<^sup>2 / (40*k)"
-      using True mult_right_mono [OF mult_mono [OF t23 t23], of "3*\<gamma> / (40*k)"] \<open>k>0\<close> by (simp add: power2_eq_square)
+      using True mult_right_mono [OF mult_mono [OF t23 t23], of "3*\<gamma> / (40*k)"] \<open>k>0\<close> 
+      by (simp add: power2_eq_square x320_def)
     finally have \<dagger>: "\<delta>*k - ok_fun_95b k \<le> 3 * \<gamma> * (real t)\<^sup>2 / (40*k)" .
 
     have "gamf \<gamma> \<ge> gamf (1/5)"
@@ -177,26 +180,26 @@ proof -
       by (smt (verit, best) exp_le_cancel_iff)
   next
     case False
-    have "\<delta> * k - ok_fun_95b k \<le> (\<gamma>/30) * k"
-      using \<open>k>0\<close> ok95 by (simp add: \<delta>_def)
-    also have "\<dots> \<le> 3 * \<gamma> * (real t)\<^sup>2 / (40*k)"
+    then have "ok_fun_95b k + \<gamma>*k / 15 \<ge> k/200"
+      using big \<open>k\<ge>l\<close> by (auto simp: Big_Far_10_2_def Big_Far_9_2_def)
+    with \<open>k>0\<close> have "\<delta> * k - ok_fun_95b k \<le> (\<gamma>/15) * k"
+      by (simp add: \<delta>_def x320_def)    
+    also have "\<dots> \<le> 3 * \<gamma> * (real t)\<^sup>2 / (20*k)"
       using \<gamma> mult_right_mono [OF mult_mono [OF t23 t23], of "3*\<gamma> / (40*k)"] \<open>k>0\<close>
       by (simp add: power2_eq_square field_simps)
-    also have "\<dots> \<le> 3 * \<gamma> * (real t)\<^sup>2 / (20*k)"
-      using \<gamma> by (simp add: divide_simps)
     finally have \<dagger>: "\<delta>*k - ok_fun_95b k \<le> 3 * \<gamma> * (real t)\<^sup>2 / (20*k)" .
 
-    have "gamf \<gamma> \<ge> gamf (3/20 + 1/300)"
-      apply (intro DERIV_nonpos_imp_nonincreasing[of \<gamma> "3/20 + 1/300" gamf] )
+    have "gamf \<gamma> \<ge> gamf x320"
+      apply (intro DERIV_nonpos_imp_nonincreasing[of \<gamma> "x320" gamf] )
       using False apply (force simp add: )
       apply (rule deriv_gamf [of \<gamma>])
       using False \<gamma>
-         apply (auto simp: )
+         apply (auto simp: x320_def)
       done
-    moreover have "ln (gamf (3/20 + 1/300)) \<ge> -1/3 + 1/10"
-      unfolding gamf_def by (approximation 6)
-    moreover have "gamf (3/20 + 1/300) > 0"
-      by (simp add: gamf_def)
+    moreover have "ln (gamf x320) \<ge> -1/3 + 1/10"
+      unfolding gamf_def x320_def by (approximation 6)
+    moreover have "gamf x320 > 0"
+      by (simp add: gamf_def x320_def)
     ultimately have "gamf \<gamma> \<ge> exp (-1/3 + 1/10)"
       using ln_ge_iff by auto
     from powr_mono2 [OF _ _ this]
