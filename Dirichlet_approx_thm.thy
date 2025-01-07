7a6b24050444bd1ac5d6b6a876a0f1de9328344a
paulson <lp15@cam.ac.uk>
2025-01-07 12:11:43 +0000

    Tidying in preparation for a possible blog post

--- Dirichlet_approx_thm.thy
+++ Dirichlet_approx_thm.thy
@@ -1,6 +1,6 @@
 theory Dirichlet_approx_thm
 
-imports "HOL-Complex_Analysis.Complex_Analysis"
+imports "Complex_Main" "HOL-Library.FuncSet"
 
 begin
 
@@ -11,50 +11,52 @@ text \<open>Theorem 7.1\<close>
 theorem Dirichlet_approx:
   fixes \<theta>::real and N::nat
   assumes "N > 0" 
-  obtains h k where "0 < k" "k \<le> int N" "\<bar>of_int k * \<theta> - of_int h\<bar> < 1/N"
+  obtains h k where "0 < k" "k \<le> int N" "\<bar>of_int k*\<theta> - of_int h\<bar> < 1/N"
 proof -
   have lessN: "nat \<lfloor>x * N\<rfloor> < N" if "0 \<le> x" "x < 1" for x::real
     using that assms floor_less_iff nat_less_iff by fastforce
   define X where "X \<equiv> (\<lambda>k. frac (k*\<theta>)) ` {..N}"
   define Y where "Y \<equiv> (\<lambda>k::nat. {k/N..< Suc k/N}) ` {..<N}"
-  have False if non: "\<forall>a b::nat. b \<le> N \<longrightarrow> a < b \<longrightarrow> \<not> \<bar>frac (b * \<theta>) - frac (a * \<theta>)\<bar> < 1/N"
+  have False 
+    if non: "\<not> (\<exists>b\<le>N. \<exists>a<b. \<bar>frac (real b * \<theta>) - frac (real a * \<theta>)\<bar> < 1/N)"
   proof -
-    have "inj_on (\<lambda>k::nat. frac (k * \<theta>)) {..N}"
-      using that by (intro linorder_inj_onI) fastforce+
+    have "inj_on (\<lambda>k::nat. frac (k*\<theta>)) {..N}"
+      using non by (intro linorder_inj_onI; force)
     then have caX: "card X = Suc N"
       by (simp add: X_def card_image)
     have caY: "card Y \<le> N" "finite Y"
       unfolding Y_def using card_image_le by force+
-    define f where "f \<equiv> \<lambda>x::real. let k = nat \<lfloor>x * N\<rfloor> in {k/N..< Suc k/N}"
+    define f where "f \<equiv> \<lambda>x::real. let k = nat \<lfloor>x * N\<rfloor> in {k/N ..< Suc k/N}"
     have "f \<in> X \<rightarrow> Y"
-      by (force simp: f_def X_def Y_def Let_def Ico_eq_Ico frac_lt_1 divide_simps intro!: lessN)
+      by (force simp: f_def Let_def X_def Y_def frac_lt_1 intro!: lessN)
     then have "\<not> inj_on f X"
       using \<open>finite Y\<close> caX caY card_inj by fastforce
     then obtain x x' where "x\<noteq>x'" "x \<in> X" "x' \<in> X" and eq: "f x = f x'"
       by (auto simp: inj_on_def)
-    then obtain c c'::nat where c: "c \<noteq> c'" "c \<le> N" "c' \<le> N" 
-            and xeq: "x = frac (c * \<theta>)" and xeq': "x' = frac (c' * \<theta>)"
+    then obtain c d::nat where c: "c \<noteq> d" "c \<le> N" "d \<le> N" 
+            and xeq: "x = frac (c*\<theta>)" and xeq': "x' = frac (d*\<theta>)"
       by (metis (no_types, lifting) X_def atMost_iff image_iff)
-    define k where "k \<equiv> nat \<lfloor>x * N\<rfloor>"
-    then have k: "x \<in> {k/N..< Suc k/N}" 
+    define i where "i \<equiv> nat \<lfloor>x * N\<rfloor>"
+    then have i: "x \<in> {i/N ..< Suc i/N}" 
       using assms by (auto simp: divide_simps xeq) linarith
-    have k': "x' \<in> {k/N..< Suc k/N}" 
-      using eq assms by (simp add: f_def Let_def divide_simps xeq' k_def) linarith
-    with assms k have "\<bar>frac (c' * \<theta>) - frac (c * \<theta>)\<bar> < 1 / real N"
+    have i': "x' \<in> {i/N ..< Suc i/N}" 
+      using eq assms unfolding f_def Let_def xeq' i_def
+      by (simp add:  divide_simps) linarith
+    with assms i have "\<bar>frac (d*\<theta>) - frac (c*\<theta>)\<bar> < 1 / N"
       by (simp add: xeq xeq' abs_if add_divide_distrib)
-    then show False
-      by (metis \<open>c \<le> N\<close> \<open>c \<noteq> c'\<close> \<open>c' \<le> N\<close> abs_minus_commute nat_neq_iff non)
+    with c show False
+      by (metis abs_minus_commute nat_neq_iff non)
   qed
-  then obtain a b::nat where "a<b" "b \<le> N" and *: "\<bar>frac (b * \<theta>) - frac (a * \<theta>)\<bar> < 1/N"
-    by blast
-  let ?k = "b-a" and ?h = "\<lfloor>b * \<theta>\<rfloor> - \<lfloor>a * \<theta>\<rfloor>"
+  then obtain a b::nat where *: "a<b" "b\<le>N" "\<bar>frac (b*\<theta>) - frac (a*\<theta>)\<bar> < 1/N" 
+    by metis
+  let ?k = "b-a" and ?h = "\<lfloor>b*\<theta>\<rfloor> - \<lfloor>a*\<theta>\<rfloor>"
   show ?thesis
   proof
-    have "frac (b * \<theta>) - frac (a * \<theta>) = ?k * \<theta> - ?h"
-      using \<open>a < b\<close> by (simp add: frac_def left_diff_distrib' of_nat_diff)
-    then show "\<bar>of_int ?k * \<theta> - ?h\<bar> < 1 / N"
-      by (metis * of_int_of_nat_eq)
-  qed (use \<open>a < b\<close> \<open>b \<le> N\<close> in auto)
+    have "frac (b*\<theta>) - frac (a*\<theta>) = ?k*\<theta> - ?h"
+      using \<open>a < b\<close> by (simp add: frac_def left_diff_distrib')
+    with * show "\<bar>of_int ?k*\<theta> - ?h\<bar> < 1 / N"
+      by (metis of_int_of_nat_eq)
+  qed (use * in auto)
 qed
 
 
@@ -62,9 +64,10 @@ text \<open>Theorem 7.2\<close>
 corollary Dirichlet_approx_coprime:
   fixes \<theta>:: real and N:: nat
   assumes "N > 0" 
-  obtains h k where "coprime h k" "0 < k" "k \<le> int N" "\<bar>of_int k * \<theta> - of_int h\<bar> < 1/N"
+  obtains h k where "coprime h k" "0 < k" "k \<le> int N" "\<bar>of_int k*\<theta> - of_int h\<bar> < 1/N"
 proof -
-  obtain h' k' where k': "0 < k'" "k' \<le> int N" and *: "\<bar>of_int k' * \<theta> - of_int h'\<bar> < 1/N"
+  obtain h' k' where k': "0 < k'" "k' \<le> int N" 
+    and *: "\<bar>of_int k'*\<theta> - of_int h'\<bar> < 1/N"
     by (meson Dirichlet_approx assms)
   let ?d = "gcd h' k'"
   show thesis
@@ -82,7 +85,7 @@ proof -
       show "coprime (?h::int) ?k"
         by (metis "1" div_gcd_coprime gcd_eq_0_iff not_one_less_zero)
       show k0: "0 < ?k"
-        using \<open>k'>0\<close> div_positive_int by auto
+        using k'(1) pos_imp_zdiv_pos_iff by force
       show "?k \<le> int N"
         using k' "1" int_div_less_self by fastforce
       have "\<bar>\<theta> - of_int ?h / of_int ?k\<bar> = \<bar>\<theta> - of_int h' / of_int k'\<bar>"
@@ -92,7 +95,7 @@ proof -
       also have "\<dots> < 1 / of_int (N * ?k)"
         using assms \<open>k'>0\<close> k0 1
         by (simp add: frac_less2 int_div_less_self)
-      finally show "\<bar>of_int ?k * \<theta> - of_int ?h\<bar> < 1 / real N"
+      finally show "\<bar>of_int ?k*\<theta> - of_int ?h\<bar> < 1 / real N"
         using k0 k' by (simp add: field_simps)
     qed
   qed
